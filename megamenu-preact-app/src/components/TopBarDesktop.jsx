db18e16bffb6829c66e7a5fe7eb99efc581e92be
mattpettefer <mattpettefer@gmail.com>
2025-04-30 18:14:04 -0400

    desktop styles close

--- megamenu-preact-app/src/components/TopBarDesktop.jsx
+++ megamenu-preact-app/src/components/TopBarDesktop.jsx
@@ -61,48 +61,55 @@ class TopBarDesktop extends Component {
       <div className="top-bar-desktop">
         {/* Left side - LogoBar HTML */}
         <div className="topbar-logobar">
-          <div className="crest">
-            <img src="https://life.edu/wp-content/themes/life/images/lifecrest.jpg" />
+          <div className="crest2025">
+            <img src="https://life.edu/wp-content/themes/life/images/lifecrest-green.jpg" />
           </div>
-          <h1 className="banner-logo">
-            <a className="logo2020" href="/" title="Home"><i className="icon-logo"></i></a>
+          <h1 className="banner-logo2025">
+            <a className="logo2025" href="/" title="Life University"><i className="icon-logo"></i></a>
           </h1>
         </div>
 
-        {/* Center - Small menu displayed horizontally */}
-        <div className="small-menu-container">
-          <ul className="small-menu">
-            {data.smallMenu.items.map(item => (
-              <li key={item.id}>
-                <a href={item.url}>{item.title}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
+        {/* Center & Right - Menus container */}
+        <div className="topbar-menus-container">
+          <div className="search-area2025">
+            <form id="searchForm2025">
+              <input id="jsSearchValue" type="text" placeholder="SEARCH" />
+            </form>
+            <a id="jsSearchButton" href="#"><i className="icon-search2025"></i></a>
+          </div>
+          <div className="small-menu-container">
+            <ul className="small-menu">
+              {data.smallMenu.items.map(item => (
+                <li key={item.id}>
+                  <a href={item.url}>{item.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        {/* Right side - Dashboards dropdown */}
-        <div className="dashboard-menu-container" ref={ref => this.dashboardRef = ref}>
-          <a 
-            href="#" 
-            className={`dashboard-toggle ${isDashboardOpen ? 'active' : ''}`}
-            onClick={this.toggleDashboard}
-            aria-expanded={isDashboardOpen}
-            aria-haspopup="true"
-          >
-            Dashboards
-            <span className="dropdown-indicator" aria-hidden="true"></span>
-          </a>
-          {isDashboardOpen && (
-            <div className="dashboard-dropdown">
-              <ul>
-                {data.dashboards.items.map(item => (
-                  <li key={item.id}>
-                    <a href={item.url}>{item.title}</a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
+          <div className="dashboard-menu-container" ref={ref => this.dashboardRef = ref}>
+            <a 
+              href="#" 
+              className={`dashboard-toggle2025 ${isDashboardOpen ? 'active' : ''}`}
+              onClick={this.toggleDashboard}
+              aria-expanded={isDashboardOpen}
+              aria-haspopup="true"
+            >
+              EEE
+              <span className="dropdown-indicator2025" aria-hidden="true"></span>
+            </a>
+            {isDashboardOpen && (
+              <div className="dashboard-dropdown2025">
+                <ul>
+                  {data.dashboards.items.map(item => (
+                    <li key={item.id}>
+                      <a href={item.url}>{item.title}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     );
